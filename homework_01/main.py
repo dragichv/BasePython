1b8c5f384cbf237dcbe274c6c11a13c34f302255
Рале Виталий Эдуардович <V.Rale@otpbank.ru>
2021-09-23 23:20:45 +0300

    1120

--- homework_01/main.py
+++ homework_01/main.py
@@ -15,14 +15,27 @@ def power_numbers(*numbers):
         list_num.append(i)
     return list_num
 
+power_numbers(1, 2, 4, 5, 7, 8, 10, 11)
+
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def filter_odd(num):
+     if(num % 2) != 0:
+         return True
+     else:
+         return False
 
-def filter_numbers():
+def filter_even(num):
+    if(num % 2) == 0:
+        return True
+    else:
+        return False
+
+def filter_numbers(num, var):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -32,4 +45,16 @@ def filter_numbers():
     <<< [1, 3]
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
-    """
+    """  
+    if var == 'odd':
+        odd_n = filter(filter_odd, num)
+        return list(odd_n)
+    elif var == 'even':
+        even_n = filter(filter_even, num)
+        return list(even_n)
+    
+filter_numbers([1, 2, 4, 5, 7, 8, 10, 11], ODD)
+
+
+
+
